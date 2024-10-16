6eda3c45b8735ffedc844c14953620f82f4186f1
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-16 13:23:22 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -21,6 +21,6 @@ docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
 
 docker rm -f aura-frontend || true
 
-# docker network create aura-network
+docker network create aura-network
 
 sudo docker run -d -p 80:80 --name aura-frontend --network aura-network 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
