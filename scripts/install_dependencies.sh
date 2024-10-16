5cbc8f4f495af2cad5de7cc1c9bf446b3f7fdedf
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-16 18:43:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -20,7 +20,7 @@ echo "$TOKEN" | sudo docker login --username AWS --password-stdin 270514764245.d
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
 
 docker rm -f aura-frontend || true
-
+ 
 docker network create aura-network
 
 sudo docker run -d -p 80:80 --name aura-frontend --network aura-network 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
