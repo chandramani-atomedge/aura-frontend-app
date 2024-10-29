d80ab9cd05283a5776469073692b2108b96691f8
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-29 12:55:28 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -18,17 +18,17 @@ echo "$TOKEN" | sudo docker login --username AWS --password-stdin 270514764245.d
 
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
 
-docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest || true
+# docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest || true
  
-NETWORK_NAME="aura-network"
+# NETWORK_NAME="aura-network"
 
-# Check if the network already exists
-if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
-    echo "Network '$NETWORK_NAME' already exists."
-else
-    # Create the network if it doesn't exist
-    docker network create "$NETWORK_NAME"
-    echo "Network '$NETWORK_NAME' created."
-fi
+# # Check if the network already exists
+# if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
+#     echo "Network '$NETWORK_NAME' already exists."
+# else
+#     # Create the network if it doesn't exist
+#     docker network create "$NETWORK_NAME"
+#     echo "Network '$NETWORK_NAME' created."
+# fi
 
 #sudo docker run -d -p 80:80 --name aura-frontend --network aura-network 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
