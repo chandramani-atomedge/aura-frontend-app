55f223498852242a5cca64c4cf853a2dced1e8f0
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-11-14 15:40:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,27 +8,13 @@ sudo apt-get install unzip
 sudo systemctl start docker
 sudo systemctl enable docker
    
-# Log in to AWS ECR
-TOKEN=$(aws ecr get-login-password --region us-east-1)
-if [ -z "$TOKEN" ]; then
-    echo "Failed to get ECR login token" >&2
-    exit 1
-fi
-echo "$TOKEN" | sudo docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
 
-docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-middleware:latest
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
 
-# docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest || true
- 
-# NETWORK_NAME="aura-network"
+IMAGE_TAG=$(aws ssm get-parameter --name "frontend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
 
-# # Check if the network already exists
-# if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
-#     echo "Network '$NETWORK_NAME' already exists."
-# else
-#     # Create the network if it doesn't exist
-#     docker network create "$NETWORK_NAME"
-#     echo "Network '$NETWORK_NAME' created."
-# fi
+# docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-middleware:latest || true
 
-#sudo docker run -d -p 80:80 --name aura-frontend --network aura-network 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
+docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-middleware:$IMAGE_TAG
+
+docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:$IMAGE_TAG
