b1644d6c3e9438c75c71f014ba262c9113931c54
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-11-21 12:38:47 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,10 +11,10 @@ sudo systemctl enable docker
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
 
-IMAGE_TAG=$(aws ssm get-parameter --name "frontend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
+# IMAGE_TAG=$(aws ssm get-parameter --name "frontend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
 
 # docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-middleware:latest || true
 
 # docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-middleware:$IMAGE_TAG
 
-docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:$IMAGE_TAG
+docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-frontend:latest
